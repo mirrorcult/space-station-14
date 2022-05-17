a329d09415e7c768b99c3f19d193dffca10ee5a4
Kara D <lunarautomaton6@gmail.com>
2022-05-16 23:35:19 -0700

    funny

---
+++ Content.Client/Viewcone/ViewconeSystem.cs
@@ -0,0 +1,28 @@
+﻿using Robust.Client.GameObjects;
+
+namespace Content.Client.Viewcone;
+
+public sealed class ViewconeSystem : EntitySystem
+{
+    private EntityUid _coneEntity;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PlayerAttachedEvent>(OnPlayerAttached); ;
+    }
+
+    private void OnPlayerAttached(PlayerAttachedEvent ev)
+    {
+        var xform = Transform(ev.Entity);
+        if (_coneEntity != EntityUid.Invalid)
+            Del(_coneEntity);
+
+        _coneEntity = Spawn("WallViewcone", xform.Coordinates);
+        var coneXform = Transform(_coneEntity);
+        coneXform.AttachParent(ev.Entity);
+        coneXform.LocalRotation = Angle.Zero;
+        coneXform.Coordinates = new(ev.Entity, 0f, 1.1f);
+    }
+}
